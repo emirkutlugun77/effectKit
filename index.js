c4af4b823d0c4358a7345d361da9d2f26e3088b9
emirkutlugun77 <57583899+emirkutlugun77@users.noreply.github.com>
2021-04-07 23:43:02 +0300

    akasya duragi

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ for (var z = 0; z < length; z++) {
                 var sound = new Audio("sounds/tom_4.mp3");
                 sound.play();
                 break;
+            case "e":
+                var sound = new Audio("sounds/sasirma.mp3");
+                sound.play();
+                break;
+            case "r":
+                var sound = new Audio("sounds/wololo.mp3");
+                sound.play();
+                break;
             default:
                 break;
         }
@@ -70,43 +78,15 @@ document.addEventListener("keydown", function(event) {
             var sound = new Audio("sounds/tom_4.mp3");
             sound.play();
             break;
+        case "e":
+            var sound = new Audio("sounds/sasirma.mp3");
+            sound.play();
+            break;
+        case "r":
+            var sound = new Audio("sounds/wololo.mp3");
+            sound.play();
+            break;
         default:
             break;
     }
 });
-
-function playWdrum() {
-
-    var sound = Audio("sounds/crash.mp3");
-    sound.play();
-}
-
-function playBass() {
-    var sound = Audio("sounds/kick-bass.mp3");
-    sound.play();
-}
-
-function playSnare() {
-    var sound = Audio("sounds/snare.mp3");
-    sound.play();
-}
-
-function playTom1() {
-    var sound = Audio("sounds/tom_1.mp3");
-    sound.play();
-}
-
-function playTom2() {
-    var sound = Audio("sounds/tom_2.mp3");
-    sound.play();
-}
-
-function playTom3() {
-    var sound = Audio("sounds/tom_3.mp3");
-    sound.play();
-}
-
-function playTom4() {
-    var sound = Audio("sounds/tom_4.mp3");
-    sound.play();
-}
